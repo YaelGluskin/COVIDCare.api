b654fe8add3a81c760c479d04c29f9618bcad296
Yael Gluskin <yaelgluskin@gmail.com>
2024-03-21 21:29:27 +0200

    Cookies

--- ServerSide/server.js
+++ ServerSide/server.js
@@ -3,12 +3,14 @@ const app = express()
 const path = require('path')
 const { logger } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
 const PORT = process.env.PORT || 3500
 // The logger wull be before everything else
 app.use(logger)
 // Receive and parse json data
 app.use(express.json())
-
+// Receive and parse cookies
+app.use(cookieParser())
 // Navigate to the public
 app.use('/', express.static(path.join(__dirname, 'public')))
 
