d3ebd0c343c7c647252524bc58d4adcc09dec2f4
Yael Gluskin <yaelgluskin@gmail.com>
2024-03-26 00:47:58 +0200

    Set useGetUsersQuery

--- clientside/src/features/users/UsersList.js
+++ clientside/src/features/users/UsersList.js
@@ -1,4 +1,12 @@
+import { useGetUsersQuery } from "./usersApiSlice"
 const UsersList = () => {
+    const {
+       data: useres,
+       isLoading,
+       isSuccess,
+       isError,
+       error  
+    } = useGetUsersQuery();
     return (
         <h1>UsersList</h1>
     )
