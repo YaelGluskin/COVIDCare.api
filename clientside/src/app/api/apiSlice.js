4b8e541264119dcd83a495afefcbf683d4361bd9
Yael Gluskin <yaelgluskin@gmail.com>
2024-03-26 00:28:45 +0200

    Set redux

---
+++ clientside/src/app/api/apiSlice.js
@@ -0,0 +1,15 @@
+import{createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+export const apiSlice = createApi({
+    // The fetchBaseQuery function is imported from '@reduxjs/toolkit/query/react'.
+    // It is used to configure how API requests are made, including specifying the base URL.
+    // In this case, it sets the base URL to 'http://localhost:3500'.
+    fetchBaseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
+
+    // The tagTypes array is provided to categorize different types of entities returned by the API.
+    tagTypes: ['User', 'Client', 'Vaccine', 'Disease'],
+
+    // The endpoints object is provided to define various API endpoints.
+    // Endpoints are defined using the builder argument passed to the function.
+    endpoints: builder => ({})
+});
---
+++ clientside/src/app/store.js
@@ -0,0 +1,22 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./api/apiSlice";
+import { buildGetDefaultMiddleware } from "@reduxjs/toolkit/dist/getDefaultMiddleware";
+
+// Configure the Redux store using the configureStore function from "@reduxjs/toolkit".
+export const store = configureStore({
+    // The reducer object defines the initial state and the functions to handle actions for different parts of the state.
+    // In this case, the apiSlice.reducer is assigned to a specific key in the state.
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer
+    },
+
+    // The middleware option allows for customizing the middleware stack.
+    // In this case, the getDefaultMiddleware function is used to obtain the default middleware stack,
+    // and then the apiSlice.middleware is appended to it using the concat method.
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
+
+    // The devTools option enables the Redux DevTools Extension, which provides advanced debugging capabilities.
+    // It is set to true to enable the extension in the browser.
+    devTools: true
+});
