c9286b7d23421b7213c4f11be994db3b31381595
Yael Gluskin <yaelgluskin@gmail.com>
2024-03-26 16:12:02 +0200

    Create Disease component and DiseaseApiSlice

--- clientside/src/features/disease/DiseasesList.js
+++ clientside/src/features/disease/DiseasesList.js
@@ -1,4 +1,13 @@
+import { useGetDiseasesQuery } from "./diseasesApiSlice"
+import Disease from "./Disease"; // Import the Disease component
 const DiseasesList = () => {
+    const {
+        data: diseases, // Rename 'data' to 'diseases' for clarity
+        isLoading,
+        isSuccess,
+        isError,
+        error  
+     } = useGetDiseasesQuery();
     return (
         <h1>DiseasesList</h1>
     )
