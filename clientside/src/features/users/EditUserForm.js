5419fc2bc2ad7139dd8442c76a58dfd73081214a
Yael Gluskin <yaelgluskin@gmail.com>
2024-03-27 01:14:12 +0200

    Create newUser and EditUser form

---
+++ clientside/src/features/users/EditUserForm.js
@@ -0,0 +1,157 @@
+import { useState, useEffect } from "react"
+import { useUpdateUserMutation, useDeleteUserMutation} from "./usersApiSlice"
+import { useNavigate } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSave, faTrash } from "@fortawesome/free-solid-svg-icons"
+import { ROLES } from "../../config/roles"
+import {USER_REGEX, PWD_REGEX}  from "../../config/pwd"
+
+const EditUserForm = ({user}) => {
+    const [updateUser, 
+        // Deliver the status after call to the func
+        {isLoauding, isSuccess, isError, err
+        }] = useUpdateUserMutation();
+
+    const [deleteUser,{
+        isSuccess: isDeleteSuccess ,
+        isError: isDeleteError, 
+        err: deleteError
+    }] = useDeleteUserMutation();
+    
+    const nav = useNavigate();
+
+    const [username, setUsername] = useState(user.username)
+    const [validUsername, setValidUsername] = useState(false)
+    const [password, setPassword] = useState('')
+    const [validPassword, setValidPassword] = useState(false)
+    const [roles, setRoles] = useState(user.roles)
+    const[activestatus, setActivestatus] = useState(user.activestatus) // For disable an employee
+
+    useEffect(() => {
+        setValidUsername(USER_REGEX.test(username))
+    }, [username])
+
+    useEffect(() => {
+        setValidPassword(PWD_REGEX.test(password))
+    }, [password])
+
+    useEffect(() => { 
+        console.log(isSuccess)
+        if (isSuccess || isDeleteSuccess) { // Checking the 2 Mutation at once
+            setUsername('')
+            setPassword('')
+            setRoles([])
+            nav('/dash/users')
+        }
+
+    }, [isSuccess, isDeleteSuccess, nav])
+
+    const onPwdChanged = e => setPassword(e.target.value)  
+    const onNameChanged = e => setUsername(e.target.value)
+    const onRolesChanged = e => { // Allowes more than one option to be selected
+        const values = Array.from( // The array takes the values, and create an array stored in values        
+            e.target.selectedOptions, (option) => option.value
+        ) 
+        setRoles(values) // Set the roles to the values
+    }
+    const onActChange = () => setActivestatus(prev => !prev) // Oppsite the chosen
+
+    const onSaveUserClicked = async (e) => {
+        if (password) { // It possible to update user detail with/wuthout the password
+            await updateUser({ id: user.id, username, password, roles, activestatus })
+        } else {
+            await updateUser({ id: user.id, username, roles, activestatus })
+        }
+    }
+
+    const onDeleteUserClicked = async () => {
+        await deleteUser({ id: user.id })
+    }
+
+    const options= Object.values(ROLES).map(role => {
+        return ( <option key={role} value={role} > {role} </option > )
+    })
+    let goSave // Save with pwd or not.
+    if (password) {
+        goSave = [roles.length, validUsername, validPassword].every(Boolean) && !isLoauding
+    } else {
+        goSave = [roles.length, validUsername].every(Boolean) && !isLoauding
+    }
+    const errClass= (isError || isDeleteError) ? "errmsg" : "offscreen"
+    const validUserClass = !validUsername ? 'form__input--incomplete' : ''
+    const validPwdClass= password && !validPassword ? 'form__input--incomplete' : ''
+    const validRolesClass= !Boolean(roles.length) ? 'form__input--incomplete' : ''
+
+    const errContent = (err?.data?.message || deleteError?.data?.message) ?? ''
+
+
+    const content = (
+        <>
+            {/* If errors accur, the mesgge from the back-end will display*/}
+      <p className={errClass}>{errContent}</p> 
+
+      <form className="General_Form" onSubmit={e=> e.preventDefault()}>
+        <div className="General_Form__title-row">
+          <h2>Edit User</h2>
+          <div className="General_Form__action-buttons">
+            <button className="custom-icon-button"
+              title="Save"
+              onClick={onSaveUserClicked}
+              disabled={!goSave}
+            >
+              <FontAwesomeIcon icon={faSave} />
+            </button>
+            <button className="icon-button" title="Delete" onClick={onDeleteUserClicked} >
+                <FontAwesomeIcon icon={faTrash} /> </button>
+          </div>
+        </div>
+
+        <label className="General_Form__label" htmlFor="username">
+          Username: <span className="nowrap">[3-20 letters]</span>
+        </label>
+        <input
+          className={`General_Form__input ${validUserClass}`}
+          id="username"
+          name="username"
+          type="text"
+          autoComplete="off"
+          value={username}
+          onChange={onNameChanged}
+        />
+
+        <label className="General_Form__label" htmlFor="password">
+          Password: <span className="nowrap">[4-12 chars incl. !@#$%]</span>
+        </label>
+        <input
+          className={`General_Form__input ${validPwdClass}`}
+          id="password"
+          name="password"
+          type="password"
+          value={password}
+          onChange={onPwdChanged}
+        />
+
+        <label className="form__label form__checkbox-container" htmlFor="user-active">
+            ACTIVE: 
+            <input className="form__checkbox" id="user-active"
+                name="user-active" type="checkbox" checked={activestatus}
+                onChange={onActChange} /> 
+        </label>
+
+        <label className="General_Form__label" htmlFor="roles">
+          ASSIGNED ROLES:
+        </label>
+        <select id="roles" name="roles"
+          className={`General_Form__select ${validRolesClass}`}
+          multiple={true} size="3" value={roles}
+          onChange={onRolesChanged} >
+          {options}
+        </select>
+      </form>
+        </>
+    )
+
+  return content;
+}
+
+export default EditUserForm
--- clientside/src/features/users/NewUser.js
+++ clientside/src/features/users/NewUser.js
@@ -1,8 +1,127 @@
-// Placeholder for routing
+import { useState, useEffect } from "react"
+import { useAddNewUserMutation } from "./usersApiSlice"
+import { useNavigate } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSave } from "@fortawesome/free-solid-svg-icons"
+import { ROLES } from "../../config/roles"
+import {USER_REGEX, PWD_REGEX}  from "../../config/pwd"
+
 const NewUser = () => {
-  return (
-    <div>NewUser</div>
+  // Unlike a query, It is called when we calle it (and not immediately)
+  // addNewUser - Gives an add new user fnction
+  const [addNewUser, 
+    // Deliver the status after call to the func
+    {isLoauding, isSuccess, isError, err}] = useAddNewUserMutation();
+  const nav = useNavigate();
+
+  const [username, setUsername] = useState('') // 
+  const [validUsername, setValidUsername] = useState(false)
+  const [password, setPassword] = useState('')
+  const [validPassword, setValidPassword] = useState(false)
+  const [roles, setRoles] = useState(["Employee"])
+
+  useEffect(() => { 
+    setValidUsername(USER_REGEX.test(username)) 
+  }, [username])
+
+    useEffect(() => {
+        setValidPassword(PWD_REGEX.test(password))
+    }, [password])
+
+    useEffect(() => {
+        if (isSuccess) {
+            setUsername('')
+            setPassword('')
+            setRoles([])
+            nav('/dash/users') // Otherwith a warning
+        }
+    }, [isSuccess, nav])
+
+  const onPwdChanged = e => setPassword(e.target.value)  
+  const onNameChanged = e => setUsername(e.target.value)
+  const onRolesChanged = e => {
+    // Allowes more than one option to be selected
+    const values = Array.from( // The array takes the values, and create an array stored in values        
+      e.target.selectedOptions, (option) => option.value )
+    setRoles(values) // Set the roles to the values
+  }
+  // Set all to boolean by sticking all to the array 'goSave', it will be true if we are not louding
+  const goSave = [roles.length, validUsername, validPassword].every(Boolean) && !isLoauding;
+
+  const onSaveUserClicked = async (e) => {
+    e.preventDefault()
+    if (goSave) {
+        await addNewUser({ username, password, roles }) // Call to addNewUser and sent the 3 elements.
+    }
+  } 
+
+  const options = Object.values(ROLES).map(role => {
+    return ( <option key={role} value={role} > {role} </option > )
+  })
+
+  // Classes for no apply
+  const errClass = isError ? "errmsg" : "offscreen"
+  const validUserClass = !validUsername ? 'form__input--incomplete' : ''
+  const validPwdClass = !validPassword ? 'form__input--incomplete' : ''
+  const validRolesClass = !Boolean(roles.length) ? 'form__input--incomplete' : '' // If Yes, we didnt aplly nothing
+
+  let content = (
+    <>
+      {/* If errors accur, the mesgge from the back-end will display*/}
+      <p className={errClass}>{err?.data?.message}</p> 
+      <form className="General_Form" onSubmit={onSaveUserClicked}>
+        <div className="General_Form__title-row">
+          <h2>New User</h2>
+          <div className="General_Form__action-buttons">
+            <button
+              className="custom-icon-button"
+              title="Save"
+              disabled={!goSave}
+            >
+              <FontAwesomeIcon icon={faSave} />
+            </button>
+          </div>
+        </div>
+        <label className="General_Form__label" htmlFor="username">
+          Username: <span className="nowrap">[3-20 letters]</span>
+        </label>
+        <input
+          className={`General_Form__input ${validUserClass}`}
+          id="username"
+          name="username"
+          type="text"
+          autoComplete="off"
+          value={username}
+          onChange={onNameChanged}
+        />
+
+        <label className="General_Form__label" htmlFor="password">
+          Password: <span className="nowrap">[4-12 chars incl. !@#$%]</span>
+        </label>
+        <input
+          className={`General_Form__input ${validPwdClass}`}
+          id="password"
+          name="password"
+          type="password"
+          value={password}
+          onChange={onPwdChanged}
+        />
+
+        <label className="General_Form__label" htmlFor="roles">
+          ASSIGNED ROLES:
+        </label>
+        <select id="roles" name="roles"
+          className={`General_Form__select ${validRolesClass}`}
+          multiple={true} size="3" value={roles}
+          onChange={onRolesChanged} >
+          {options}
+        </select>
+      </form>
+
+    </>
   )
+
+  return content;
 }
 
 export default NewUser
