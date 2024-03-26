26eea52b15cce0c0984fe4e1bfd83d6de982d0d9
Yael Gluskin <yaelgluskin@gmail.com>
2024-03-26 15:38:12 +0200

    Set Table of vaccine

--- clientside/src/features/vaccine/Vaccine.js
+++ clientside/src/features/vaccine/Vaccine.js
@@ -3,15 +3,24 @@ import { faPenToSquare } from "@fortawesome/free-solid-svg-icons"
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { selectVaccineById } from './vaccinesApiSlice'
+// import { selectClientById } from '../clients/clientsApiSlice'
 
 // React component representing a vaccine row in a table
 const Vaccine = ({ vaccineId }) => {
     const vaccine = useSelector(state => selectVaccineById(state, vaccineId)) // Select vaccine from Redux store by vaccineId
+    console.log(vaccine)
     const navigate = useNavigate() // Hook to navigate to different routes
-
+    // Assuming selectClientById is your Redux selector to fetch a client by ID
+    /*
+    const clientId = vaccine ? vaccine.client : null;
+    const client = useSelector(state => selectClientById(state, clientId))
+    console.log(clientId) 
+    console.log(client) 
+    */
     if (vaccine) { // If vaccine exists, render vaccine details
         // Function to handle edit button click, navigates to vaccine edit page
         const handleEdit = () => navigate(`/dash/vaccines/${vaccineId}`)
+        
         // Convert vaccine roles array to string for display
         // const vaccineRolesDisplay = vaccine.roles.toString().replaceAll(',', ', ') // For looking good
         // const cellStatus = vaccine.active ? '' : 'table__cell--inactive'  // Determine cell status based on vaccine's active status
@@ -21,7 +30,7 @@ const Vaccine = ({ vaccineId }) => {
             <tr className="table__row vaccine">
                 <td className="table__cell accineDate">{vaccineDate}</td>
                 <td className={`table__cell `}>{vaccine.name}</td>
-                <td className={`table__cell `}>{vaccine.clientID}</td>
+                <td className={`table__cell `}>{vaccine.clientName} , id: {vaccine.clientID}</td>
                 <td className={`table__cell `}>
                     {/* Edit button */}
                     <button
