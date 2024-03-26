4a9510ce8bc7a2f003b50a332e66693d0c5efc38
Yael Gluskin <yaelgluskin@gmail.com>
2024-03-27 00:39:29 +0200

    Add Links

--- clientside/src/features/auth/Welcome.js
+++ clientside/src/features/auth/Welcome.js
@@ -6,9 +6,18 @@ const Welcom =() => {
         <section className="welcome">
             <p>{today}</p>
             <h1>Welcome!</h1>
-            <p>
-                <Link to="/dash/users">View User Settings</Link>
-            </p>
+            <p><Link to="/dash/users">View User Settings</Link></p>
+            <p><Link to="/dash/users/new">  Create new User</Link></p>
+
+            <p><Link to="/dash/clients">View Current Clients</Link></p>
+            <p><Link to="/dash/clients/new">    Create new Client</Link></p>
+
+            <p><Link to="/dash/vaccines">View Vaccines</Link></p>
+            <p><Link to="/dash/vaccines/new">   Create new Vaccine</Link></p>
+
+            <p><Link to="/dash/diseases">View Disease</Link></p>
+            <p><Link to="/dash/diseases/new">   Create new Disease</Link></p>
+            
         </section>
     )
     return content;
