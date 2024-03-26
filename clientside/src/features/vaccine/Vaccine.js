95d3a341dd7921931d01891ea303260266afae70
Yael Gluskin <yaelgluskin@gmail.com>
2024-03-26 13:43:29 +0200

    Set Vaccine

--- clientside/src/features/vaccine/Vaccine.js
+++ clientside/src/features/vaccine/Vaccine.js
@@ -6,7 +6,36 @@ import { selectVaccineById } from './vaccinesApiSlice'
 
 // React component representing a vaccine row in a table
 const Vaccine = ({ vaccineId }) => {
+    const vaccine = useSelector(state => selectVaccineById(state, vaccineId)) // Select vaccine from Redux store by vaccineId
+    const navigate = useNavigate() // Hook to navigate to different routes
 
+    if (vaccine) { // If vaccine exists, render vaccine details
+        // Function to handle edit button click, navigates to vaccine edit page
+        const handleEdit = () => navigate(`/dash/vaccines/${vaccineId}`)
+        // Convert vaccine roles array to string for display
+        // const vaccineRolesDisplay = vaccine.roles.toString().replaceAll(',', ', ') // For looking good
+        // const cellStatus = vaccine.active ? '' : 'table__cell--inactive'  // Determine cell status based on vaccine's active status
+        const vaccineDate = new Date(vaccine.date).toLocaleString('en-IL', { day: 'numeric', month: 'long', year: 'numeric' })
+        // const clientId = new String(vaccine.client)
+        return ( // Render vaccine row
+            <tr className="table__row vaccine">
+                <td className="table__cell accineDate">{vaccineDate}</td>
+                <td className={`table__cell `}>{vaccine.name}</td>
+                <td className={`table__cell `}>{vaccine.clientID}</td>
+                <td className={`table__cell `}>
+                    {/* Edit button */}
+                    <button
+                        className="icon-button table__button"
+                        onClick={handleEdit}
+                    >
+                        {/* Edit icon */}
+                        <FontAwesomeIcon icon={faPenToSquare} />
+                    </button>
+                </td>
+            </tr>
+        )
+
+    } else return null  // If vaccine does not exist, return null (no rendering)
 };
 
 export default Vaccine;
