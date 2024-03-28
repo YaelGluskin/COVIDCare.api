da026e6eceebd40bb4fde0050d4a07a148497056
Yael Gluskin <yaelgluskin@gmail.com>
2024-03-28 03:07:20 +0200

    auth controller

---
+++ ServerSide/routes/authRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express') // Importing the Express module
+const router = express.Router() // Creating a router object from the Express Router
+// Importing the userController module which contains CRUD operations for users
+const authController = require('../controllers/authController');
+const loginLim = require('../middleware/logger')
+
+router.route('/') // The root Route, already in /auth
+    .post(loginLim, authController.login) // placeholder
+
+router.route('/refresh') // 
+    .get(authController.refresh)
+
+router.route('/logout') // 
+    .post(authController.logout) // placeholder
+
+module.exports = router
--- ServerSide/server.js
+++ ServerSide/server.js
@@ -26,6 +26,7 @@ app.use(cookieParser())
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
+app.use('/auth', require('./routes/userRoutes')) // Add auth routes to server
 app.use('/users', require('./routes/userRoutes'))
 app.use('/clients', require('./routes/clientRoutes'))
 app.use('/vaccines', require('./routes/vacRoutes'))
