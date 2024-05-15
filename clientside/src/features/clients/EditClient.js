f3942472141835218afeeb7455c637d112c1dbf9
Yael Gluskin <145065202+YaelGluskin@users.noreply.github.com>
2024-05-15 16:21:17 +0300

    Update EditClient.js

--- clientside/src/features/clients/EditClient.js
+++ clientside/src/features/clients/EditClient.js
@@ -6,7 +6,6 @@ import EditClientForm from './EditClientForm'
 const EditClient = () => {
   const {id} =useParams()
   const client = useSelector(state => selectClientById(state, id))
-  console.log(client)
   const content =client? <EditClientForm client={client} /> :<p> Louding ...</p>
   return content;
 }
