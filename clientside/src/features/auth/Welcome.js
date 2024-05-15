16e21f70c4f8bd5908eac20bddaa880d14bca5d7
Yael Gluskin <145065202+YaelGluskin@users.noreply.github.com>
2024-05-15 16:10:25 +0300

    Update Welcome.js

--- clientside/src/features/auth/Welcome.js
+++ clientside/src/features/auth/Welcome.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import useAuth from '../../hooks/useAuth';
 
 const Welcom = () => {
     const date = new Date();
-    const today = new Intl.DateTimeFormat('en-IL', { dateStyle: 'medium', timeStyle: 'long' }).format(date);
-
+    const today = new Intl.DateTimeFormat('en-IL', { dateStyle: 'long', timeStyle: 'short' }).format(date);
+    const {username, isManager, isAdmin} = useAuth()
+    
     return (
         <section className="welcome">
             <p>{today}</p>
-            <h1>Welcome!</h1>
-            <p><Link to="/dash/users">View User Settings</Link></p>
+            <h1>Welcome {username} !</h1>
+            
+            {(isAdmin || isManager ) && ( <div> 
+            <p><Link to="/dash/users">View Users</Link></p>
             <p><Link to="/dash/users/new">Create new User</Link></p>
-
+            <br></br> 
+            </div>)}
             <p><Link to="/dash/clients">View Current Clients</Link></p>
-            <p><Link to="/dash/clients/new">Create new Client</Link></p>
 
+            <p><Link to="/dash/clients/new">Create new Client</Link></p>
+            <br></br>
             <p><Link to="/dash/vaccines">View Vaccines</Link></p>
             {/* <p><Link to="/dash/vaccines/new">Create new Vaccine</Link></p> */}
 
