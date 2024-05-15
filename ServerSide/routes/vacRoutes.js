b781e7561c52fdab5c7840c3ab260972d9b4c8d8
Yael Gluskin <145065202+YaelGluskin@users.noreply.github.com>
2024-05-15 18:32:00 +0300

    Apply JWT verification middleware to vaccines

--- ServerSide/routes/vacRoutes.js
+++ ServerSide/routes/vacRoutes.js
@@ -2,9 +2,9 @@ const express = require('express') // Importing the Express module
 const router = express.Router() // Creating a router object from the Express Router
 // Importing the vaccineController module which contains CRUD operations for vaccines
 const vaccineController = require('../controllers/vaccineController');
-// const verifyJWT = require('../middleware/verifyJWT')
+const verifyJWT = require('../middleware/verifyJWT')
 
-// router.use(verifyJWT) // Apply JWT verification middleware to all routes
+router.use(verifyJWT) // Apply JWT verification middleware to all routes
 
 router.route('/') // Define routes for CRUD operations on vaccines
     .get(vaccineController.getVaccines)     // GET request to retrieve all vaccines
