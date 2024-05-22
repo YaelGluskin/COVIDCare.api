3e608b4d485889ad27717ac836f82c9194dbbd84
Yael Gluskin <yaelgluskin@gmail.com>
2024-05-22 12:42:35 +0300

    Remove the server path

--- config/originsAllowed.js
+++
@@ -1,7 +0,0 @@
-// List of allowed origins includes fiction website for Securing CORS
-const originsAllowed = [
-    'https://covidcare.onrender.com'            
-];
-
-// Exporting the list of allowed origins for use in other modules
-module.exports = originsAllowed;
